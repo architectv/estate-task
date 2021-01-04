053ab75aa852eba6ca899c47d90f47d2bb1a3df9
Alexey Vasyukov <a.vasyukov1@yandex.ru>
2021-01-04 23:53:28 +0300

    Add postgres migrations

---
+++ scripts/000001_init.up.sql
@@ -0,0 +1,12 @@
+CREATE TABLE rooms (
+    id serial PRIMARY KEY,
+    description text NOT NULL,
+    price int NOT NULL CHECK (price > 0)
+);
+
+CREATE TABLE bookings (
+    id serial PRIMARY KEY,
+    room_id int REFERENCES rooms (id) ON DELETE CASCADE NOT NULL,
+    date_start date NOT NULL,
+    date_end date NOT NULL
+);
