533418f93660b1a57ff2a0fc13490adce834122e
Alexey Vasyukov <48162713+architectv@users.noreply.github.com>
2021-01-08 11:15:24 +0300

    Update 000001_init.up.sql

--- scripts/000001_init.up.sql
+++ scripts/000001_init.up.sql
@@ -8,5 +8,5 @@ CREATE TABLE bookings (
     id serial PRIMARY KEY,
     room_id int REFERENCES rooms (id) ON DELETE CASCADE NOT NULL,
     date_start date NOT NULL,
-    date_end date NOT NULL
+    date_end date NOT NULL CHECK (date_start < date_end)
 );
